a03ea2555b2a7ccc95fd25ce3fbdcba3bcf08656
An Pham <vuadenthuj1@gmail.com>
2019-06-03 15:32:11 +0700

    Update: add action test message page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const request = require('request');
 var cors = require('cors');
 const dbConfig = require('./src/config');
 
@@ -40,17 +41,22 @@ app.post('/webhook', (req, res) => {
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function (entry) {
 
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
 
-            if (entry.messaging && entry.messaging[0]) {
-                let webhook_event = entry.messaging[0];
 
-                // Get the sender PSID
-                let sender_psid = webhook_event.sender.id;
-                console.log('Sender PSID: ' + sender_psid);
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
             }
-            console.log('entry.messaging', entry.messaging);
         });
 
         // Returns a '200 OK' response to all requests
@@ -98,15 +104,65 @@ app.listen(port, () => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Checks if the message contains text
+    if (received_message.text) {
+
+        // Creates the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            'text': `You sent the message: '${received_message.text}'. Now send me an attachment!`
+        };
+
+    } else if (received_message.attachments) {
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        console.log('attachment_url', attachment_url);
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { 'text': 'Thanks!' };
+    } else if (payload === 'no') {
+        response = { 'text': 'Oops, try sending another image.' };
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        'recipient': {
+            'id': sender_psid
+        },
+        'message': response
+    };
 
+    // Send the HTTP request to the Messenger Platform
+    request({
+        'uri': 'https://graph.facebook.com/v2.6/me/messages',
+        'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+        'method': 'POST',
+        'json': request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error('Unable to send message:' + err);
+        }
+    });
 }
